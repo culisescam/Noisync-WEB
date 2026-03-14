5c039ea597f87dbce2265506a91b429afc64d1ee
Ulises Campuzano Hernandez <20243ds023@utez.edu.mx>
2026-03-13 23:19:23 -0600

    Correccion en import

--- frontend/src/features/admin/components/inviteMusicianCard.jsx
+++ frontend/src/features/admin/components/inviteMusicianCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../../api/api";
 import { getInstruments } from "../../../api/instrumentService";
 
