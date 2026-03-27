531d2b0f8df5a4c935392206c7bc687081f249b2
Ulises Campuzano Hernandez <20243ds023@utez.edu.mx>
2026-03-27 16:49:42 -0600

    Agregar header ngrok

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -4,6 +4,7 @@ export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080", 
   headers: {
     "Content-Type": "application/json",
+       "ngrok-skip-browser-warning": "true" 
   },
 });
 
